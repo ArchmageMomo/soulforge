bd986e3b4685bd95d79bcd3e42fea173267924fc
Nachbagauer-tgm <32197576+Nachbagauer-tgm@users.noreply.github.com>
2017-12-08 12:50:29 +0100

    Fix the Problem by renamig the function
    https://www.reddit.com/r/themoddingofisaac/comments/5nszb8/info_error_in_postevaluateitems_call_attempt_to/

--- main.lua
+++ main.lua
@@ -1,35 +1,38 @@
 local Soulforge = RegisterMod("Soulforge",1);
-local BumboSoul = Isaac.GetItemIdByName ("Bum-bo Soul");
+local BumboSoul = Isaac.GetItemIdByName ("BumBo Soul");
 
-function Soulforge:CacheUpdate(player, CacheFlag)
-  player = Isaac.GetPlayer(0);
-  if player:HasCollectible(BumboSoul) then 
-    random = math.random(1,5);
+
+function Soulforge:CacheUpdate(player, cacheFlag)
+  local player = Isaac.GetPlayer(0);
+  
+  if player:HasCollectible(BumboSoul) == true then 
+    local random = math.random(1,5);
     if (random <=1 )then
-      if CacheFlag == CacheFlag.CACHE_DAMAGE then
+      if cacheFlag == CacheFlag.CACHE_DAMAGE then
         player.Damage = player.Damage +1;
       end
     end
     if (random > 1 and random <= 2 )then
-      if CacheFlag == CacheFlag.CACHE_SPEED then
+      if cacheFlag == CacheFlag.CACHE_SPEED then
         player.MoveSpeed = player.MoveSpeed +1;
       end
     end
     if (random > 2 and random <= 3 )then
-      if CacheFlag == CacheFlag.CACHE_SHOTSPEED then
+      if cacheFlag == CacheFlag.CACHE_SHOTSPEED then
         player.ShotSpeed = player.ShotSpeed+1;
       end
     end
     if (random > 3 and random <= 4 )then
-      if CacheFlag == CacheFlag.CACHE_RANGE then
+      if cacheFlag == CacheFlag.CACHE_RANGE then
         player.TearHeight = player.TearHeight +1;
       end
     end 
     if (random > 4 and random <= 5 )then
-      if CacheFlag == CacheFlag.CACHE_LUCK then
+      if cacheFlag == CacheFlag.CACHE_LUCK then
         player.Luck = player.Luck +1;
       end
     end
   end
 end
-Soulforge:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Soulforge.CachUpdate);
+Isaac.DebugString("Help")
+Soulforge:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Soulforge.CacheUpdate);
