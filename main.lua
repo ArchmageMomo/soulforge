1e28af9aff3433af1072418bec5dd80cdc1e86c3
Nachbagauer-tgm <32197576+Nachbagauer-tgm@users.noreply.github.com>
2017-12-20 11:09:51 +0100

    BugFix
    Corrected values
    Add Coin funtion (not finished)
    Add new Item to XML
    Change Spread Size

--- main.lua
+++ main.lua
@@ -2,25 +2,35 @@ local Soulforge = RegisterMod("Soulforge",1);
 local BumboSoul = Isaac.GetItemIdByName ("BumBo Soul");
 
 
+function Soulforge:PickUp ()
+    if Soulforge.PICKUP_COIN == true then 
+      if cacheFlag == CacheFlag.CACHE_DAMAGE then
+        player.Damage = player.Damage +1;
+      end
+    end
+  end
+
+
 function Soulforge:CacheUpdate(player, cacheFlag)
   local player = Isaac.GetPlayer(0);
-   
+  
   if player:HasCollectible(BumboSoul) == true then 
    
     local random = math.random(0,6);
+    if (random >= 0 and random < 6) then
     if (random <=1 )then
       if cacheFlag == CacheFlag.CACHE_DAMAGE then
-        player.Damage = player.Damage +0,3;
+        player.Damage = player.Damage + 1;
       end
     end
     if (random > 1 and random <= 2 )then
       if cacheFlag == CacheFlag.CACHE_SPEED then
-        player.MoveSpeed = player.MoveSpeed + 0,2;
+        player.MoveSpeed = player.MoveSpeed + 1;
       end
     end
     if (random > 2 and random <= 3 )then
       if cacheFlag == CacheFlag.CACHE_SHOTSPEED then
-        player.ShotSpeed = player.ShotSpeed+ 0,2;
+        player.ShotSpeed = player.ShotSpeed+ 1;
       end 
     end
     if (random > 3 and random <= 4 )then
@@ -33,7 +43,7 @@ function Soulforge:CacheUpdate(player, cacheFlag)
         player.Luck = player.Luck +1;
       end
     end
-  
+    end
   end
 end
 
