99cb8aebeb71b3d60a2709515ec77d7c1ca8d562
Nachbagauer-tgm <32197576+Nachbagauer-tgm@users.noreply.github.com>
2018-01-10 15:51:08 +0100

    Commit between
    Error at line 3
    [INFO] - ERR: ...mes/Binding of Isaac Afterbirth+ Mods/soulforge/main.lua:3: attempt to call a nil value (field 'GetItemByName')

--- main.lua
+++ main.lua
@@ -1,74 +1,62 @@
 local Soulforge = RegisterMod("Soulforge",1);
 local BumboSoul = Isaac.GetItemIdByName ("BumBo Soul");
-local AngleSoul = Isaac.GetItemIdByName ("Angle Soul");
+local FlameThrower = Isaac.GetItemByName ("Flamethrower");
+--ArcaneLockdown: Nachtrefferfolge auf selben gegner explodieren aus ihm (zerstört die gegner nicht) Tears(Player)
+--Flamethrower: Flammenwerfer
+--Angle Soul:HP(ethernal Harts) pro flooor Angle Deal 
 
-
-
-
-function Soulforge:SetValue ()
-  local player =Isaac.GetPlayer(0)
-  values = {player.Hearts, player.Damage, player.ShotSpeed, player.TearHeight, player.MoveSpeed, player.Luck} 
-end
-
-function Soulforge:PickUp ()
-    if Soulforge.PICKUP_COIN == true then 
-      if cacheFlag == CacheFlag.CACHE_DAMAGE then
-        player.Damage = player.Damage +1;
-      end
-    end
-  end
-
-
-function Soulforge:CacheUpdate(player, cacheFlag)
-  local player = Isaac.GetPlayer(0);
-  local values = {} 
-  player.Hearts, player.Damage, player.ShotSpeed, player.TearHeight, player.MoveSpeed, player.Luck
-  h = player.Hearts
-  d = player.Damage
-  s = player.ShotSpeed
-  t = player.TearHeight
-  m = player.MoveSpeed
-  l = player.Luck
+function Soulforge:CacheUpdate(player, CacheFlag)
   
-  values[h
+  local player = Isaac.GetPlayer(0);
   local random = math.random(0,6)
-  if player:HasCollectible(BumboSoul) == true then 
-    
-   
-    if (random <=1 )then
-      if cacheFlag == CacheFlag.CACHE_DAMAGE then
-        values [1] = values[1]+1
-      end
-    end
-    if (random > 1 and random <= 2 )then
-      if cacheFlag == CacheFlag.CACHE_SPEED then
-        values [4] = values [4]+1
-      end
-    end
-    if (random > 2 and random <= 3 )then
-      if cacheFlag == CacheFlag.CACHE_SHOTSPEED then
-        values [2] = values [2]+1
-      end 
-    end
-    if (random > 3 and random <= 4 )then
-      if cacheFlag == CacheFlag.CACHE_RANGE then
-        values [5] = values [5]+1
-      end
-    end 
-    if (random > 4 and random <= 5 )then
-      if cacheFlag == CacheFlag.CACHE_LUCK then
-        values [5] = values [5]+1
-      end
-    end
-    
+  
+  if player:HasCollectible(FlameThrower) == true then 
+    Isaac.GetPlayer(0).TearColor = Color(0.003921568627451,0.010752688172043,0,0,0,0,0)
   end
-  player.Damage =values [1]
-  player.ShotSpeed =values [2]
-  player.TearHeight =values [3]
-  player.MoveSpeed =values [4]
-  player.Luck =values [5]
+
+  if player:HasCollectible(BumboSoul) == true then 
+
+    local random = math.random(0,6);
+    if (random >= 0 and random < 6) then
+      if (random <=1 )then
+        if cacheFlag == CacheFlag.CACHE_DAMAGE then
+          player.Damage=player.Damage+1.0
+        end
+      end
+      if (random > 1 and random <= 2 )then
+        if cacheFlag == CacheFlag.CACHE_SPEED then
+          player.MoveSpeed=player.MoveSpeed+1.0
+        end
+      end
+      if (random > 2 and random <= 3 )then
+        if cacheFlag == CacheFlag.CACHE_SHOTSPEED then
+          player.ShotSpeed=player.ShotSpeed+1.0
+        end 
+      end
+      if (random > 3 and random <= 4 )then
+        if cacheFlag == CacheFlag.CACHE_RANGE then
+          player.TearHeight = player.TearHeight +1.0 
+        end
+      end 
+      if (random > 4 and random <= 5 )then
+        if cacheFlag == CacheFlag.CACHE_LUCK then
+          player.Luck = player.Luck+1.0
+        end
+      end
+
+    end
+
+end
 end
 
-Soulforge:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Soulforge.CacheUpdate);
-Soulforge:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, Soulforge.SetValue);
-Soulforge:AddCallback(ModCallbacks.MC_LEVEL_GENERATOR, Soulforge.SetValue);
+
+function Soulforge:Color()
+    local player= Isaac.GetPlayer(0)
+    if Isaac.GetPlayer(0):HasCollectible(FlameThrower) then
+      Isaac.GetPlayer(0).TearColor = Color(0.003921568627451,0.010752688172043,0.0,0,0,0,0)
+    end
+  end
+
+  Soulforge:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Soulforge.CacheUpdate)
+  Soulforge:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Soulforge.Color)
+  Soulforge:AddCallback(ModCallbacks.MC_POST_UPDATE, Soulforge.Color)
