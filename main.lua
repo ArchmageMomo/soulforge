5378cce6d53f3b3999ebff39c9b0c43b288a4614
Nachbagauer-tgm <32197576+Nachbagauer-tgm@users.noreply.github.com>
2018-01-10 19:48:00 +0100

    For testing

--- main.lua
+++ main.lua
@@ -5,31 +5,20 @@ local FlameThrower = Isaac.GetItemIdByName ("Item1")
 --Flamethrower: Flammenwerfer
 --Angle Soul:HP(ethernal Harts) pro flooor Angle Deal 
 
-function bit(p)
-     return 1 << p
-end
-
-function hasbit(x, p)
-    return (x & p)
-end
-
-function setbit(x, p)
-    return x | p
-end
 
 function Soulforge:CacheUpdate(player, CacheFlag)
   
   local player = Isaac.GetPlayer(0);
   local random = math.random(0,6)
+  local fd = Isaac.GetPlayer(0).MaxFireDelay / 2;
   
   if player:HasCollectible(FlameThrower) == true then 
     Isaac.GetPlayer(0).TearColor = Color(255.0,93,0,1,1,0,0)
     Isaac.GetPlayer(0).Damage = 3
-    Isaac.GetPlayer(0).FireDelay = Isaac.GetPlayer(0).FireDelay -2
+    Isaac.GetPlayer(0).FireDelay = fd-1
     Isaac.GetPlayer(0).TearHeight = 2
     Isaac.GetPlayer(0).TearFlags = Isaac.GetPlayer(0).TearFlags + TearFlags.TEAR_PIERCING + TearFlags.TEAR_BURN
     
- 
   end
 
   if player:HasCollectible(BumboSoul) == true then 
