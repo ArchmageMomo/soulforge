5cc2ee49161cd2676198eb832354f69d58c475c3
Nachbagauer-tgm <32197576+Nachbagauer-tgm@users.noreply.github.com>
2018-01-16 15:03:34 +0100

    Finall name changes

--- main.lua
+++ main.lua
@@ -1,13 +1,19 @@
 local Soulforge = RegisterMod("Soulforge",1);
 local BumboSoul = Isaac.GetItemIdByName ("BumBo Soul")
-local FlameThrower = Isaac.GetItemIdByName ("Item1")
-local AngleSoul = Isaac.GetItemIdByName ("Item2")
-local DemonSoul = Isaac.GetitemIdByName ("Item3")
+local FlameThrower = Isaac.GetItemIdByName ("Flamespitter")
+local AngleSoul = Isaac.GetItemIdByName ("Angel Soul")
+
+local repItem1 = true
 
 
---ArcaneLockdown: Nachtrefferfolge auf selben gegner explodieren aus ihm (zerstört die gegner nicht) Tears(Player)
---Flamethrower: Flammenwerfer (check)
---Angle Soul:HP(ethernal Harts) pro flooor Angle Deal 
+--BumboSoul: Gives a random Stat up
+--Flamethrower: A fucking Flamethrower what would you expect
+--Angle Soul:Grants Isaac an Ethernal Heart per flooor 
+
+--this funktions sets the boolean false if the player has the Item
+function Soulforge:Reset()
+    repItem1 = true
+end
 
 
 function Soulforge:CacheUpdate(player, cacheFlag)
@@ -22,19 +28,25 @@ function Soulforge:CacheUpdate(player, cacheFlag)
     isStatChanged = 1 
   end
   
+  -- This Code let's me change the Tearrate 
   if player:HasCollectible(FlameThrower) == true and isStatChanged == 1  then
     player.MaxFireDelay = player.MaxFireDelay - (fd/4)
     isStatChanged = 0
   end
   
-  if player:HasCollectible(FlameThrower) == true then 
+  -- Code for the Flamethrower
+  if player:HasCollectible(FlameThrower) and repItem1 == true then 
     Isaac.GetPlayer(0).TearColor = Color(255.0,93,0,1,1,0,0)
     Isaac.GetPlayer(0).Damage = 3
     Isaac.GetPlayer(0).FireDelay = fd-1
     Isaac.GetPlayer(0).TearHeight = 2
     Isaac.GetPlayer(0).TearFlags = Isaac.GetPlayer(0).TearFlags +             TearFlags.TEAR_PIERCING + TearFlags.TEAR_BURN
+    pos = Vector(Isaac.GetPlayer(0).Position.X, Isaac.GetPlayer(0).Position.Y);
+    Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, CollectibleType.COLLECTIBLE_PYROMANIAC, pos, Vector(0,0), Isaac.GetPlayer(0))
+    repItem1 = false
   end
-
+  
+  -- This code is for Bumbo Soul
   if player:HasCollectible(BumboSoul) == true then 
     player.Damage=player.Damage+math.random(0,1)*0.5;
     player.MoveSpeed=player.MoveSpeed+math.random(0,1)*0.5;
@@ -42,46 +54,30 @@ function Soulforge:CacheUpdate(player, cacheFlag)
     player.TearHeight = player.TearHeight +math.random(0,1)*0.3;
     player.Luck = player.Luck+math.random(0,1)*0.5;
 end
-  if Isaac.GetPlayer(0):HasCollectible(AngleSoul) and heart == true then 
-    
-   pos = Vector(Isaac.GetPlayer(0).Position.X, player.Position.Y);
-   Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_HEART,  HeartSubType.HEART_ETERNAL, pos, Vector(0, 0), Isaac.GetPlayer(0))
-   heart=false
-  end   
+  
   
 end
 
 
+
+--This function is just an additionsal if the tearcolor changes 
 function Soulforge:Color()
     local player= Isaac.GetPlayer(0)
     if Isaac.GetPlayer(0):HasCollectible(FlameThrower) then
     Isaac.GetPlayer(0).TearColor = Color(255.0,93,0,1,1,0,0)
     end
   end
-  
+-- This function Gives Isaac one Ethernal heart each floor
 function Soulforge:GiveHeart()
   if Isaac.GetPlayer(0):HasCollectible(AngleSoul) == true then 
-    pos = Vector(Isaac.GetPlayer(0).Position.X, Isaac 
-      .Position.Y);
+    pos = Vector(Isaac.GetPlayer(0).Position.X, Isaac.GetPlayer(0).Position.Y);
     Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_HEART,  HeartSubType.HEART_ETERNAL, pos, Vector(0, 0), Isaac.GetPlayer(0))
 
   end
 end
 
-function Soulforge:demonSoulFkt()
-  if Isaac.GetPlayer(0):HasCollectible(DemonSoul) then 
-    if PickupVariant == PickupVariant.PICKUP_HEART then
-      ran = math.random(0,6)
-      if ran > 3 then 
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_HEART,  HeartSubType.HEART_BLACK, pos, Vector(0, 0), Isaac.GetPlayer(0))
-      else
-        Isaac.GetPlayer(0).HitPoints = Isaac.GetPlayer(0).HitPoints-1
-    end
-  end
-end
-end
   Soulforge:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Soulforge.CacheUpdate)
   Soulforge:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Soulforge.Color)
   Soulforge:AddCallback(ModCallbacks.MC_POST_UPDATE, Soulforge.Color)
   Soulforge:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, Soulforge.GiveHeart)
-  Soulforge:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, Soulforge.demmonSoulFkt)
+  Soulforge:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, Soulforge.Reset)
