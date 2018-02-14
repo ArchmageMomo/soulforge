faa715130d24370853f23cbb0ac8d9c9b60b0800
Nachbagauer-tgm <32197576+Nachbagauer-tgm@users.noreply.github.com>
2018-02-14 12:10:31 +0100

    Registerd all Items in the main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,10 @@ local Soulforge = RegisterMod("Soulforge",1);
 local BumboSoul = Isaac.GetItemIdByName ("BumBo Soul")
 local FlameThrower = Isaac.GetItemIdByName ("Flamespitter")
 local AngleSoul = Isaac.GetItemIdByName ("Angel Soul")
+local DemonSoul = Isaac.GetItemIdByName ("Demon Soul")
+local DarkSoul = Isaac.GetItemIdByName ("Dark Soul")
+local StainedSoul = Isaac.GetItemIdByName ("Stained Soul")
+local PureSoul = Isaac.GetItemIdByName ("Pure Soul")
 
 local repItem1 = true
 
