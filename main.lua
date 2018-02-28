a0e1e67ace271dc0b43a4addcfa678ccb9e40d90
Momo <mnetrwal@student.tgm.ac.at>
2018-02-28 11:24:41 +0100

    add

--- main.lua
+++ main.lua
@@ -78,7 +78,7 @@ end
 
   
   -- This code is for DarkSoul
-  if Isaac.GetPlayer(0):HasCollectible(DarkSoul) == true then
+  if Isaac.GetPlayer(0):HasCollectible(item) == true then
     Isaac.GetPlayer(0).TearColor = Color(255.0,93,0,1,1,0,0)
     Isaac.GetPlayer(0).Damage=Isaac.GetPlayer(0).Damage+math.random(0,100)
       
