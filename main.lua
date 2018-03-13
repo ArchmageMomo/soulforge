1f97ff9cb7d84b532bbccc953c63f80f19b83e57
Momo <mnetrwal@student.tgm.ac.at>
2018-03-13 10:53:51 +0100

    Test 4

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ local BumboSoul = Isaac.GetItemIdByName ("BumBo Soul")
 local FlameThrower = Isaac.GetItemIdByName ("Flamespitter")
 local AngleSoul = Isaac.GetItemIdByName ("Angel Soul")
 local DemonSoul = Isaac.GetItemIdByName ("Demon Soul")
-local DarkSouling= Isaac.GetItemIdByName ("Darkest Soul")
+local DarkSoul= Isaac.GetItemIdByName ("Dark Soul")
 local StainedSoul = Isaac.GetItemIdByName ("Stained Soul") -- Sample Image
 local PureSoul = Isaac.GetItemIdByName ("Pure Soul") -- Sample Image
 
@@ -70,10 +70,21 @@ end
 --DarkSoul Function
 function Soulforge:darksoulF()
   -- This code is for DarkSoul
-  if Isaac.GetPlayer(0):HasCollectible(DarkSouling) == true then
-    Isaac.GetPlayer(0).TearColor = Color(255.0,93,0,1,1,0,0) --In Final entfernen
-      
+  if Isaac.GetPlayer(0):HasCollectible(DarkSoul) == true then
     
+    player.Damage=player.Damage+math.random(0,1)*0.5;
+    player.MoveSpeed=player.MoveSpeed+math.random(0,1)*0.5;
+    player.ShotSpeed=player.ShotSpeed+math.random(0,1)*0.2;
+    player.TearHeight = player.TearHeight +math.random(0,1)*0.3;
+    player.Luck = player.Luck+math.random(0,1)*0.5;
+    
+    --Isaac.GetPlayer(0).TearColor = Color(255.0,93,0,1,1,0,0) --In Final entfernen
+      
+    --if math.random(0,100) < 30 then
+    --  Isaac.GetPlayer(0):AddHealth(-1)
+    --else 
+    --  Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_HEART,  HeartSubType.HEART_BLACK, pos, Vector(0, 0), Isaac.GetPlayer(0))
+    --end
   end
 end
 
