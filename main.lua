10b57974110bd13cdac88196dd5f4efbbb04f47c
Momo <mnetrwal@student.tgm.ac.at>
2018-03-13 10:38:01 +0100

    Test2

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ local BumboSoul = Isaac.GetItemIdByName ("BumBo Soul")
 local FlameThrower = Isaac.GetItemIdByName ("Flamespitter")
 local AngleSoul = Isaac.GetItemIdByName ("Angel Soul")
 local DemonSoul = Isaac.GetItemIdByName ("Demon Soul")
-local DarkSouling= Isaac.GetItemIdByName ("Dark Soul")
+local DarkSouling= Isaac.GetItemIdByName ("MyItem")
 local StainedSoul = Isaac.GetItemIdByName ("Stained Soul") -- Sample Image
 local PureSoul = Isaac.GetItemIdByName ("Pure Soul") -- Sample Image
 
